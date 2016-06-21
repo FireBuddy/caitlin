428e73ffbf17d56625184bacc46614c45280c4d0
FireBuddy <aijun@live.ca>
2016-06-21 12:08:36 -0400

    Update Program.cs

--- CaitlynTheTroll/CaitlynTheTroll/Program.cs
+++ CaitlynTheTroll/CaitlynTheTroll/Program.cs
@@ -50,7 +50,7 @@ namespace CaitlynTheTroll
             Q = new Spell.Skillshot(SpellSlot.Q, 1240, SkillShotType.Linear, 250, 2000, 60);
             Q2 = new Spell.Skillshot(SpellSlot.Q, 1240, SkillShotType.Linear, 250, 2000, 60);
             {
-                Q2.AllowedCollisionCount <= 1;
+                Q2.AllowedCollisionCount < 2;
             }
             W = new Spell.Skillshot(SpellSlot.W, 820, SkillShotType.Circular, 500, int.MaxValue, 80);
             E = new Spell.Skillshot(SpellSlot.E, 800, SkillShotType.Linear, 250, 1600, 80);
